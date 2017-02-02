76d883c72a041dbf7539bcf5b5c00ef7b1567c06
oakbow <oakbow7@gmail.com>
2017-02-02 14:43:59 +0900

    fix circle.yml

--- lib/script/create_pull_request_if_needed.sh
+++ lib/script/create_pull_request_if_needed.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export BRANCH=bundle-update-`date -u "+%Y%m%d"`
 if [ -n `git status -sb 2> /dev/null | grep Gemfile.lock` ] ; then
-  git config --global user.email user@eample.com
-  git config --global user.name 'your name'
+  git config --global user.email oakbow7@gmail.com
+  git config --global user.name 'oakbow'
   git add Gemfile.lock
   git commit -m 'Bundle update'
   git branch -M $BRANCH
